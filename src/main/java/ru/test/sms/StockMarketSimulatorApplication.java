22d067e542e844ec3b800b095a9ef355fd50bebe
minikh <minikhv@gmail.com>
2022-04-09 19:48:31 +0300

    init repo

---
+++ src/main/java/ru/test/sms/StockMarketSimulatorApplication.java
@@ -0,0 +1,13 @@
+package ru.test.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockMarketSimulatorApplication.class, args);
+    }
+
+}
